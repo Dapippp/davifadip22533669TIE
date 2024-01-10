99cf9c94b20a0d0cc60256afcaaae57b2a32ea6a
Davif Adi Prayogo <88437267+Dapippp@users.noreply.github.com>
2024-01-10 16:29:28 +0700

    Add files via upload
    TUGAS PBO BU DYAH

---
+++ TestPolymorphism.java
@@ -0,0 +1,11 @@
+class Film {
+    private String namaFilm;
+
+    public Film(String namaFilm) {
+        this.namaFilm = namaFilm;
+    }
+
+    public String getNamaFilm() {
+        return namaFilm;
+    }
+}
